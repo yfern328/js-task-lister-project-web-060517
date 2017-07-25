51f38da7a1854c24e387d7707e1ccf9e7db7cc3f
yfern328 <yfernan2@u.rochester.edu>
2017-07-25 10:06:10 -0400

    Done.

--- js/models/list.js
+++ js/models/list.js
@@ -1,2 +1,41 @@
 // List Model
 
+const all = []
+
+function createList(){
+  let listId = 0
+  return class List {
+    constructor(title) {
+      this.title = title
+      this.id = listId++
+      all.push(this)
+      this.tasks = []
+    }
+
+    static get all() {
+      return all
+    }
+
+  }
+}
+
+// class List {
+//   constructor(title){
+//     this.title = title;
+//   }
+// }
+//
+// function List(title) {
+//   this.title = title
+//   this.all = []
+// }
+
+// function createList() {
+//   return class List {
+//     constructor(title){
+//       this.title = title
+//     }
+//   }
+// }
+
+let List = createList()
--- js/models/task.js
+++ js/models/task.js
@@ -1,2 +1,16 @@
 // Task Model
 
+
+function createTask(){
+  let taskId = -2
+  return class Task {
+    constructor(description, priority = "high", list){
+      this.description = description
+      this.priority = priority
+      this.id = taskId++
+      this.list = list
+    }
+  }
+}
+
+let Task = createTask()
